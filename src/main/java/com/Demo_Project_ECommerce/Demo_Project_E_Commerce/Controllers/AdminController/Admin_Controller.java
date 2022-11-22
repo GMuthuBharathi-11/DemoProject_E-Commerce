e0e34d2f9569f5078277eaeb693dab31ec0881ea
muthubharathi123 <muthu@gmail.com>
2022-11-22 17:53:19 +0530

    Admin Api

---
+++ src/main/java/com/Demo_Project_ECommerce/Demo_Project_E_Commerce/Controllers/AdminController/Admin_Controller.java
@@ -0,0 +1,2 @@
+package com.Demo_Project_ECommerce.Demo_Project_E_Commerce.Controllers.AdminController;public class Admin_Controller {
+}
---
+++ src/main/java/com/Demo_Project_ECommerce/Demo_Project_E_Commerce/Services/AdminService/Admin_Service.java
@@ -0,0 +1,2 @@
+package com.Demo_Project_ECommerce.Demo_Project_E_Commerce.Services.AdminService;public class Admin_Service {
+}
